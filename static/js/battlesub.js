4fdb74b39c45956dafffb8cbaac77094cfd6ff9a
Arturo Dominguez <adominguez@nearsoft.com>
2017-02-09 22:56:38 -0700

    added instructions

--- static/js/battlesub.js
+++ static/js/battlesub.js
@@ -4,8 +4,27 @@ var tick = function() {};
 	var editor = ace.edit("editor");
 	editor.setTheme("ace/theme/monokai");
 	editor.getSession().setMode("ace/mode/javascript");
-	editor.setValue("tick = function (submarine) {\n\t// Write your code in here\n\t\n};");
-	editor.gotoLine(2);
+	editor.setValue([
+		'tick = function (submarine) {',
+		'\t/*',
+		'\tWrite your code in here',
+		'\t',
+		'\tFunctions available:',
+		'\t\tsubmarine.moveForward()',
+		'\t\tsubmarine.moveBackwards()',
+		'\t\tsubmarine.rotate(degrees)',
+		'\t\t\tMax rotation: +-15',
+		'\t\tsubmarine.getInfo()',
+		'\t\t\tReturns: { x, y, rotation, energy, minX, minY,',
+		'\t\t\t\tmaxX, maxY, maxEnergy }',
+		'\t\tsubmarine.sonar()',
+		'\t\t\tReturns the result under submarine.sonarResults',
+		'\t*/',
+		'\t',
+		'\t',
+		'};'
+	].join('\n'));
+	editor.gotoLine(16);
 
 	var windowHeight;
 
@@ -14,7 +33,7 @@ var tick = function() {};
 	var isPlaying = false;
 	var updater;
 
-	var submarine;
+	var submarine, enemySubmarine;
 
 	$(document).ready(function() {
 		windowHeight = $(window).height();
@@ -80,6 +99,16 @@ var tick = function() {};
 		ctx.fill();
 
 		var info = submarine.getInfo();
+
+		ctx.beginPath();
+		ctx.rect(20, 20, 100, 20);
+		ctx.fillStyle = '#500';
+		ctx.fill();
+		ctx.beginPath();
+		ctx.rect(20, 20, info.energy * 100 / info.maxEnergy, 20);
+		ctx.fillStyle = '#050';
+		ctx.fill();
+
 		ctx.translate(info.x, info.y);
 		ctx.rotate(Math.PI / 180 * info.rotation);
 		ctx.drawImage(subImg , -25, -25);
--- static/js/submarineFactory.js
+++ static/js/submarineFactory.js
@@ -1,25 +1,75 @@
 var submarineFactory = function(sock, px, py) {
-	var x = px, y = py, deg = 0, socket = sock, speed = 5;
+	var x = px, y = py, deg = 0, socket = sock, speed = 5,
+		lastMovement = new Date().getTime(), lastRotation = new Date().getTime(),
+		maxX = 550, maxY = 350, maxEnergy = 2000, energy;
+
+	energy = maxEnergy;
 
 	return {
 		moveForward: function() {
-			x += speed * Math.cos(Math.PI / 180 * deg);
-			y += speed * Math.sin(Math.PI / 180 * deg);
+			if(canMove()) {
+				x += speed * Math.cos(Math.PI / 180 * deg);
+				y += speed * Math.sin(Math.PI / 180 * deg);
+
+				lastMovement = new Date().getTime();
+				energy--;
+				checkPosition();
+			}
 		},
 		moveBackwards: function() {
-			x -= speed * Math.cos(Math.PI / 180 * deg);
-			y -= speed * Math.sin(Math.PI / 180 * deg);
+			if(canMove()) {
+				x -= speed * Math.cos(Math.PI / 180 * deg);
+				y -= speed * Math.sin(Math.PI / 180 * deg);
+
+				lastMovement = new Date().getTime();
+				energy--;
+				checkPosition();
+			}
 		},
 		rotate: function(degrees) {
-			deg += degrees;
+			if(canRotate()) {
+				degrees = degrees > 5 ? 5 : degrees;
+				degrees = degrees < -5 ? -5 : degrees;
+				deg += degrees;
+
+				lastRotation = new Date().getTime();
+			}
 		},
 		getInfo: function() {
 			return {
 				x: x,
 				y: y,
-				rotation: deg
+				rotation: deg,
+				energy: energy,
+				minX: 0,
+				minY: 0,
+				maxX: maxX,
+				maxY: maxY,
+				maxEnergy: maxEnergy
 			};
+		},
+		sonar: function() {
+
 		},
 		sonarResults: null
 	};
+
+	function canRotate() {
+		return getElapsedTime(lastRotation) > 40 && energy > 0;
+	}
+
+	function canMove() {
+		return getElapsedTime(lastMovement) > 40 && energy > 0;
+	}
+
+	function getElapsedTime(lastTime) {
+		return new Date().getTime() - lastTime;
+	}
+
+	function checkPosition() {
+		x = x < 0 ? 0 : x;
+		x = x > maxX ? maxX : x;
+		y = y < 0 ? 0 : y;
+		y = y > maxY ? maxY : y;
+	}
 };
